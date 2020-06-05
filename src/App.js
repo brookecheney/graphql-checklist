b5ab52e16db72ab6632bc41fb57d5b6885b1ae79
Brooklyne Cheney <brookecheney@hotmail.com>
2020-06-05 09:34:35 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,11 +9,92 @@ query getTodos {
     id
     text
   }
-}`
+}
+`;
+const TOGGLE_TODO = gql`
+
+mutation toggleTodo($id: uuid!, $done: Boolean!) {
+  update_todos(where: { id: {_eq: $id }}, _set: {done: $done}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+
+
+`;
+
+
+const ADD_TODO = gql`
+
+mutation addTodo($text: String!) {
+  insert_todos(objects: {text: $text}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+`;
+const DELETE_TODO = gql`
+mutation deleteTodo($id: uuid!) {
+  delete_todos(where: {id: {_eq: $id}}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+
+`
 
 function App() {
+  const [todoText, setTodoText] = React.useState('')
   const { data, loading, error } = useQuery(GET_TODOS);
+  const [toggleTodo] = useMutation(TOGGLE_TODO);
+  const [addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () => setTodoText("")
+
+  });
+  const [deleteTodo] = useMutation(DELETE_TODO);
+
+  async function handleToggleTodo({id, done}) {
+  const data = await toggleTodo({ variables: { id, done: !done } });
+  console.log('toggled todo', data);
+  }
  
+  async function handleAddTodo(event) {
+    event.preventDefault();
+    if(!todoText.trim()) return;
+    const data = await addTodo({
+      variables: { text: todoText },
+      refetchQueries: [{ query: GET_TODOS }]
+    });
+    console.log('added todo', data);
+  
+
+
+  }
+  async function handleDeleteTodo({ id }) {
+    const isConfirmed = window.confirm('do you want to delete this todo?')
+    if (isConfirmed) {
+   const data = await deleteTodo({ 
+     variables: { id },
+    update: cache => {
+      const prevData = cache.readQuery({ query: GET_TODOS })
+      const newTodos = prevData.todos.filter(todo => todo.id !== id);
+      cache.writeQuery({ query: GET_TODOS, data: { todos: newTodos }});
+
+    } });
+   console.log('deleted todo', data);  
+  }
+}
+
+
 
  if (loading) return <div> Loading todos...</div>;
  if (error) return <div>Error fetching todos </div>
@@ -26,25 +107,28 @@ function App() {
    aria-label="Checkmark"></span>
    
    </h1>
+   <p>(Hint: Double click to cross out todo)</p>
+   
+   
    {}
-   <form className="mb3">
+   <form onSubmit={handleAddTodo}className="mb3">
      <input
      className="Pa2 f4
      b--dashed"
      type="text"
      placeholder="Write your todo"
+     onChange={event => setTodoText(event.target.value)}
+     value={todoText}
      />
      <button className="pa2 f4 bg-dark-pink" type="submit">Create</button>
    </form>
-   
-   <div className="flex items-center justify-center flex-column">
    { }
+   <div className="flex items-center justify-center flex-column">
+ 
    {data.todos.map(todo => (
-    <p key= {todo.id}> 
-    <span className="pointer list pa1 f3">
-      {todo.text}
-    </span>
-    <button className="bg-transparent bn f4">
+    <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}> 
+    <span className={`pointer list pa1 f3 ${todo.done && "strike"}`}>{todo.text}</span>
+    <button onClick={() => handleDeleteTodo(todo)} className="bg-transparent bn f4">
       <span className="red">&times;
       </span>
       
